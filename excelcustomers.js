d93fe6630b08687353ba2d2d3908034c5f6b7e06
Zul Wasaya <zul.wasaya@gmail.com>
2014-07-21 03:27:02 +0100

    completed first phase

--- excelcustomers.js
+++ excelcustomers.js
@@ -1,11 +1,57 @@
 Customers = new Meteor.Collection("customers");
 
+Meteor.methods({
+   upimportance: function (customerId) {
+       var cust = Customers.findOne(customerId);
+       Customers.update(customerId,
+           {$set: {importance: cust.importance + 1}});
+   },
+   downimportance: function (customerId) {
+        var cust = Customers.findOne(customerId);
+        Customers.update(customerId,
+            {$set: {importance: cust.importance - 1}});
+    }
+});
+
 if (Meteor.isClient) {
     Meteor.subscribe("customers");
     Template.customersList.customer = function () {
-        return Customers.find();
+        return Customers.find({}, {sort: {importance: -1 }});
+
     };
+
+    Template.customersList.events ({
+        "click .upimportance": function () {
+        Meteor.call("upimportance", this._id);
+    },
+        "click .downimportance": function () {
+            Meteor.call("downimportance", this._id);
+    }
+  });
+
+    Template.newCustomer.events ({
+        "submit .newCustomerForm": function (evt) {
+            evt.preventDefault();
+            Customers.insert({
+                firstname: $(".firstname").val(),
+                surname: $(".surname").val(),
+                email: $(".email").val(),
+                telephone: $(".telephone").val(),
+                address: $(".address").val(),
+                postcode: $(".postcode").val(),
+                importance: $(".importance").val(),
+                comments: $(".comments").val()
+
+            }, function (err) {
+                if (! err)
+                    $("input[type=text]").val();
+            });
+        }
+
+    });
+
 }
+
 
 
 
@@ -18,5 +64,11 @@ if (Meteor.isServer) {
     Meteor.publish("customers",function () {
         return Customers.find();
 
+    });
+
+    Customers.allow({
+        insert: function (userId, customer){
+            return customer.importance < 10;
+        }
     });
 }
