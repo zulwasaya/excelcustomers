8f9faf3591539baa671674f61be523112ca72434
Zul Wasaya <zul.wasaya@gmail.com>
2015-06-22 14:24:20 +0100

    reformated

--- model.js
+++ model.js
@@ -1,86 +1,82 @@
 Customers = new Meteor.Collection("customers");
 
 
-
 Meteor.methods({
-   upimportance: function (customerId) {
-       var cust = Customers.findOne(customerId);
+  upimportance: function (customerId) {
+    var cust = Customers.findOne(customerId);
 
-       if (cust.importance < 9 )
-       {
-       Customers.update(customerId,
-           {$set: {importance: cust.importance = parseInt(cust.importance +1) }});
-       }
-   },
-   downimportance: function (customerId) {
-       var cust = Customers.findOne(customerId);
-       if (cust.importance > 1 )
-       {
-           Customers.update(customerId,
-               {$set: {importance: cust.importance = parseInt(cust.importance - 1) }});
-       }
-   }
+    if (cust.importance < 9) {
+      Customers.update(customerId,
+        {$set: {importance: cust.importance = parseInt(cust.importance + 1)}});
+    }
+  },
+  downimportance: function (customerId) {
+    var cust = Customers.findOne(customerId);
+    if (cust.importance > 1) {
+      Customers.update(customerId,
+        {$set: {importance: cust.importance = parseInt(cust.importance - 1)}});
+    }
+  }
 
 });
 
 if (Meteor.isClient) {
 
-    Meteor.subscribe("customers");
+  Meteor.subscribe("customers");
 
 
-    Template.customersList.customer = function () {
-        return Customers.find({}, {sort: {importance: -1 }});
+  Template.customersList.customer = function () {
+    return Customers.find({}, {sort: {importance: -1}});
 
-    };
+  };
 
-    Template.totalCustomers.totalNumber = function () {
+  Template.totalCustomers.totalNumber = function () {
 
-        return Customers.find().count();
+    return Customers.find().count();
 
 
-    };
+  };
 
-    Template.customersList.events ({
-        "click .upimportance": function () {
-        Meteor.call("upimportance", this._id);
+  Template.customersList.events({
+    "click .upimportance": function () {
+      Meteor.call("upimportance", this._id);
     },
-        "click .downimportance": function () {
-            Meteor.call("downimportance", this._id);
+    "click .downimportance": function () {
+      Meteor.call("downimportance", this._id);
     },
-        "click a.delete": function (e) {
-            e.preventDefault();
-            Customers.remove( this._id);
+    "click a.delete": function (e) {
+      e.preventDefault();
+      Customers.remove(this._id);
     }
 
   });
 
-    Template.newCustomer.events ({
-        "submit .newCustomerForm": function (evt) {
-            evt.preventDefault();
-            var importanceLevel = $(".importance").val();
-            if (importanceLevel > 9 || importanceLevel < 1) {importanceLevel = 1}
-            Customers.insert({
-                firstname: $(".firstname").val(),
-                surname: $(".surname").val(),
-                email: $(".email").val(),
-                telephone: $(".telephone").val(),
-                address: $(".address").val(),
-                postcode: $(".postcode").val(),
-                importance: importanceLevel,
-                comments: $(".comments").val()
+  Template.newCustomer.events({
+    "submit .newCustomerForm": function (evt) {
+      evt.preventDefault();
+      var importanceLevel = $(".importance").val();
+      if (importanceLevel > 9 || importanceLevel < 1) {
+        importanceLevel = 1
+      }
+      Customers.insert({
+        firstname: $(".firstname").val(),
+        surname: $(".surname").val(),
+        email: $(".email").val(),
+        telephone: $(".telephone").val(),
+        address: $(".address").val(),
+        postcode: $(".postcode").val(),
+        importance: importanceLevel,
+        comments: $(".comments").val()
 
-            }, function (err) {
-                if (! err)
-                    $("input[type=text]").val();
-            });
-        }
+      }, function (err) {
+        if (!err)
+          $("input[type=text]").val();
+      });
+    }
 
-    });
+  });
 
 }
-
-
-
 
 
 if (Meteor.isServer) {
@@ -88,19 +84,19 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 
-    Meteor.publish("customers",function () {
-        return Customers.find();
+  Meteor.publish("customers", function () {
+    return Customers.find();
 
-    });
+  });
 
 
-    Customers.allow({
-        insert: function (userId, customer){
-            return customer.owner !== userId;
-        },
-        remove: function (userId, customer){
-            return customer.owner !== userId;
-        }
+  Customers.allow({
+    insert: function (userId, customer) {
+      return customer.owner !== userId;
+    },
+    remove: function (userId, customer) {
+      return customer.owner !== userId;
+    }
 
-    });
+  });
 }
